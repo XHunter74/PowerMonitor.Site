4b940750faae2b3f704f06fb064ff7214a67363c
Serhiy Krasovskyy <xhunter74@gmail.com>
2019-02-10 22:08:58 +0200

    Implemented sample controller.

--- PowerMonitor/ClientApp/src/main.ts
+++ PowerMonitor/ClientApp/src/main.ts
@@ -6,7 +6,7 @@ import { environment } from './environments/environment';
 
 export function getBaseUrl() {
   //return document.getElementsByTagName('base')[0].href;
-  return "http://192.178.10.119:3000/";
+  return "http://127.0.0.1:3000/";
 }
 
 const providers = [
