310f66730750fd2c360fea8c05cccf1888e5988d
Serhiy Krasovskyy <xhunter74@gmail.com>
2019-02-17 23:08:24 +0200

    Removed sample code.

--- PowerMonitor.Site/src/app/services/services-service.ts
+++ PowerMonitor.Site/src/app/services/services-service.ts
@@ -1,21 +1,11 @@
-import { Injectable, Inject, OnInit, Optional, SkipSelf } from '@angular/core';
-import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Injectable, Inject, Optional, SkipSelf } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { UsersService } from './users-service';
 
-import { catchError, retry } from 'rxjs/operators';
 import { ISystemInfo } from '../models/sysinfo.model';
+import { ServicesUtils } from './services-utils';
 
-
-
-const httpOptions = {
-    headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-    })
-};
 
 @Injectable({
     providedIn: 'root',
@@ -35,27 +25,6 @@ export class ServicesService {
 
     }
 
-    async getSampleData(): Promise<WeatherForecast[]> {
-        const authToken = localStorage.getItem('auth_token');
-        const headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-            'Authorization': `${authToken}`
-        });
-
-        let promise = new Promise<WeatherForecast[]>((resolve, reject) => {
-            this.http
-                .get<WeatherForecast[]>(this.baseUrl + 'services', { headers })
-                .toPromise()
-                .then(data => {
-                    resolve(data);
-                })
-                .catch(e => {
-                    reject({ error: "Server error" });
-                });
-        });
-        return promise;
-    }
-
     async getSystemInfo(): Promise<ISystemInfo> {
         const authToken = localStorage.getItem('auth_token');
         const headers = new HttpHeaders({
@@ -71,34 +40,13 @@ export class ServicesService {
                     resolve(data);
                 })
                 .catch(e => {
-                    reject({ error: "Server error" });
+                    try {
+                        ServicesUtils.handleError(this.userService, e);
+                    } catch{
+                        reject({ error: 'Server error' });
+                    }
                 });
         });
         return promise;
     }
-
-    private handleError(error: HttpErrorResponse) {
-        if (error.error instanceof ErrorEvent) {
-            // A client-side or network error occurred. Handle it accordingly.
-            console.error('An error occurred:', error.error.message);
-        } else {
-            if (error.status === 401) {
-                this.userService.logout();
-                return null;
-            }
-            console.error(
-                `Backend returned code ${error.status}, ` +
-                `body was: ${error.error}`);
-        }
-        // return an observable with a user-facing error message
-        return throwError(
-            'Something bad happened; please try again later.');
-    }
-}
-
-export interface WeatherForecast {
-    dateFormatted: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
 }
