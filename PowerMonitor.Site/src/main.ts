56cec649a93a2f750d0e0306fdb5ae646d3236f6
serhiy.krasovskyy <serhiy.krasovskyy@onlini.co>
2019-02-18 09:25:29 +0200

    Changed server URL.

--- PowerMonitor.Site/src/main.ts
+++ PowerMonitor.Site/src/main.ts
@@ -6,8 +6,8 @@ import { environment } from './environments/environment';
 
 export function getBaseUrl() {
   //return document.getElementsByTagName('base')[0].href;
-  return "http://192.178.10.119:3000/";
-  // return "http://localhost:3000/";
+  // return "http://192.178.10.119:3000/";
+  return 'http://localhost:3000/';
 }
 
 const providers = [
