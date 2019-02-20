e688d07b3f5bc8731a5938e60045218497b671ed
serhiy.krasovskyy <serhiy.krasovskyy@onlini.co>
2019-02-20 09:55:15 +0200

    Implemented voltage and amperage indicators.

--- PowerMonitor.Site/src/app/home/real-data.component.ts
+++ PowerMonitor.Site/src/app/home/real-data.component.ts
@@ -14,20 +14,42 @@ export class RealDataComponent implements OnInit {
   private nominalVoltageMin = 200;
   private nominalVoltageMax = 240;
   private voltage = 230;
+  private maxAmperage = 30;
+  private nominalAmperageMin = 0;
+  private nominalAmperageMax = 20;
 
   public canvasWidth = 300;
-  public needleValue = Math.round(230 / this.maxVoltage * 100);
   public centralLabel = '';
-  public name = 'Voltage, V';
-  public bottomLabel = this.voltage.toString();
-  public options = {
-    hasNeedle: true,
-    needleColor: 'gray',
-    needleUpdateSpeed: 500,
-    arcColors: ['red', 'green', 'red'],
-    arcDelimiters: [Math.round(this.nominalVoltageMin / this.maxVoltage * 100), Math.round(this.nominalVoltageMax / this.maxVoltage * 100)],
-    rangeLabel: ['0', this.maxVoltage.toString()],
-    needleStartValue: 50,
+
+  public voltageIndicator = {
+    bottomLabel: this.voltage.toString(),
+    needleValue: Math.round(230 / this.maxVoltage * 100),
+    name: 'Voltage, V',
+    options: {
+      hasNeedle: true,
+      needleColor: 'gray',
+      needleUpdateSpeed: 500,
+      arcColors: ['red', 'green', 'red'],
+      arcDelimiters: [Math.round(this.nominalVoltageMin / this.maxVoltage * 100),
+      Math.round(this.nominalVoltageMax / this.maxVoltage * 100)],
+      rangeLabel: ['0', this.maxVoltage.toString()],
+      needleStartValue: 50,
+    }
+  };
+
+  public currentIndicator = {
+    bottomLabel: '10.0',
+    needleValue: 30,
+    name: 'Amperage, A',
+    options: {
+      hasNeedle: true,
+      needleColor: 'gray',
+      needleUpdateSpeed: 500,
+      arcColors: ['red', 'green', 'red'],
+      arcDelimiters: [0.5, Math.round(this.nominalAmperageMax / this.maxAmperage * 100)],
+      rangeLabel: ['0', this.maxAmperage.toString()],
+      needleStartValue: 50,
+    }
   };
 
   constructor(private powerService: PowerService) {
@@ -41,7 +63,7 @@ export class RealDataComponent implements OnInit {
     try {
       while (1 == 1) {
         const sensorsData = await this.powerService.getSensorsData();
-        this.updateGaugeIndicator(sensorsData);
+        this.updateGaugeIndicators(sensorsData);
         delay(1000);
       }
     } catch (e) {
@@ -49,13 +71,23 @@ export class RealDataComponent implements OnInit {
       alert('Something going wrong!');
     }
   }
-  updateGaugeIndicator(sensorsData: ISensorsDataModel) {
-    this.needleValue = Math.round(sensorsData.voltage / this.maxVoltage * 100);
-    this.bottomLabel = (Math.round(sensorsData.voltage * 10) / 10).toString();
+  updateGaugeIndicators(sensorsData: ISensorsDataModel) {
+    this.voltageIndicator.needleValue = Math.round(sensorsData.voltage / this.maxVoltage * 100);
+    this.voltageIndicator.bottomLabel = formatNumber(sensorsData.voltage);
+    this.currentIndicator.needleValue = Math.round(sensorsData.amperage / this.maxAmperage * 100);
+    this.currentIndicator.bottomLabel = formatNumber(sensorsData.amperage);
   }
 
 }
 
 async function delay(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function formatNumber(value: number): any {
+  let result = (Math.round(value * 10) / 10).toString();
+  if (!result.includes('.')) {
+    result += '.0';
+  }
+  return result;
 }
