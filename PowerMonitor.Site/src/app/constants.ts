532b947378cfc0f6568bebf1960f484d21a464c4
serhiy.krasovskyy <serhiy.krasovskyy@onlini.co>
2019-06-05 09:06:59 +0300

    Changed axis labels to month names.

--- PowerMonitor.Site/src/app/constants.ts
+++ PowerMonitor.Site/src/app/constants.ts
@@ -1,3 +1,6 @@
-export class Constans{
+export class Constans {
     static systemStartDate = new Date(2019, 1, 18);
+    static monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'];
+
 }
--- PowerMonitor.Site/src/app/power-monitor/power-monitor-monthly.component.ts
+++ PowerMonitor.Site/src/app/power-monitor/power-monitor-monthly.component.ts
@@ -176,7 +176,7 @@ export class PowerMonitorMonthlyComponent extends AppBaseComponent implements On
         if (data.length < 12) {
             for (let i = 0; i < 12; i++) {
                 chartData.push(0);
-                chartLabels.push((i + 1).toString());
+                chartLabels.push(Constans.monthNames[i]);
             }
             for (const record of data) {
                 chartData[record.month - 1] = record.power;
@@ -186,7 +186,7 @@ export class PowerMonitorMonthlyComponent extends AppBaseComponent implements On
                 return e.power;
             });
             chartLabels = data.map(e => {
-                return e.month.toString();
+                return Constans.monthNames[e.month - 1];
             });
         }
         this.barChartData[0].data = chartData;
