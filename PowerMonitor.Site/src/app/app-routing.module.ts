b4068b17a6530ae1c535021b2c7cf2ae505d5c59
serhiy.krasovskyy <xhunter74@gmail.com>
2023-01-24 19:10:41 +0200

    Disabled all features without login.

--- PowerMonitor.Site/src/app/app-routing.module.ts
+++ PowerMonitor.Site/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PowerMonitorComponent } from './power-monitor/power-monitor.component';
-import { OpenGuard, AuthGuard } from './services/auth.guard';
+import { AuthGuard, AuthGuard } from './services/auth.guard';
 import { RealDataComponent } from './real-data/real-data.component';
 import { PlatformInfoComponent } from './platform-info/platform-info.component';
 import { SiteSettingsComponent } from './site-settings/site-settings.component';
@@ -12,19 +12,19 @@ import { AppLoginComponent } from './app-login/login.component';
 import { PowerConsumptionComponent } from './power-consumption/power-consumption.component';
 
 const routes: Routes = [
-    { path: '', component: PowerMonitorComponent, data: { name: 'hourly' }, canActivate: [OpenGuard] },
+    { path: '', component: PowerMonitorComponent, data: { name: 'hourly' }, canActivate: [AuthGuard] },
     { path: 'online-data', component: RealDataComponent, pathMatch: 'full', canActivate: [AuthGuard] },
-    { path: 'platform-info', component: PlatformInfoComponent, canActivate: [OpenGuard] },
+    { path: 'platform-info', component: PlatformInfoComponent, canActivate: [AuthGuard] },
     { path: 'settings', component: SiteSettingsComponent, canActivate: [AuthGuard] },
-    { path: 'power-monitor', component: PowerMonitorComponent, data: { name: 'hourly' }, canActivate: [OpenGuard] },
-    { path: 'power-monitor/hourly', component: PowerMonitorComponent, data: { name: 'hourly' }, canActivate: [OpenGuard] },
-    { path: 'power-monitor/daily', component: PowerMonitorComponent, data: { name: 'daily' }, canActivate: [OpenGuard] },
-    { path: 'power-monitor/monthly', component: PowerMonitorComponent, data: { name: 'monthly' }, canActivate: [OpenGuard] },
-    { path: 'power-failures', component: PowerFailuresComponent, canActivate: [OpenGuard] },
-    { path: 'power-failures/daily', component: PowerFailuresComponent, data: { name: 'daily' }, canActivate: [OpenGuard] },
-    { path: 'power-failures/monthly', component: PowerFailuresComponent, data: { name: 'monthly' }, canActivate: [OpenGuard] },
-    { path: 'voltage-amperage/hourly', component: VoltageAmperageComponent, data: { name: 'hourly' }, canActivate: [OpenGuard] },
-    { path: 'voltage-amperage/daily', component: VoltageAmperageComponent, data: { name: 'daily' }, canActivate: [OpenGuard] },
+    { path: 'power-monitor', component: PowerMonitorComponent, data: { name: 'hourly' }, canActivate: [AuthGuard] },
+    { path: 'power-monitor/hourly', component: PowerMonitorComponent, data: { name: 'hourly' }, canActivate: [AuthGuard] },
+    { path: 'power-monitor/daily', component: PowerMonitorComponent, data: { name: 'daily' }, canActivate: [AuthGuard] },
+    { path: 'power-monitor/monthly', component: PowerMonitorComponent, data: { name: 'monthly' }, canActivate: [AuthGuard] },
+    { path: 'power-failures', component: PowerFailuresComponent, canActivate: [AuthGuard] },
+    { path: 'power-failures/daily', component: PowerFailuresComponent, data: { name: 'daily' }, canActivate: [AuthGuard] },
+    { path: 'power-failures/monthly', component: PowerFailuresComponent, data: { name: 'monthly' }, canActivate: [AuthGuard] },
+    { path: 'voltage-amperage/hourly', component: VoltageAmperageComponent, data: { name: 'hourly' }, canActivate: [AuthGuard] },
+    { path: 'voltage-amperage/daily', component: VoltageAmperageComponent, data: { name: 'daily' }, canActivate: [AuthGuard] },
     { path: 'profile', component: ProfileComponent, pathMatch: 'full', canActivate: [AuthGuard] },
     { path: 'power-consumption', component: PowerConsumptionComponent, canActivate: [AuthGuard] },
     { path: 'app-login', component: AppLoginComponent },
