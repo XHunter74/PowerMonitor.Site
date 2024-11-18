a1dfe7b1f982f4fc14f5f4c31424aa0f66deb995
serhiy.krasovskyy <xhunter74@gmail.com>
2024-11-18 22:49:32 +0200

    Added translation service.

--- src/app/app-initialize.factory.ts
+++ src/app/app-initialize.factory.ts
@@ -3,6 +3,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { LOCATION_INITIALIZED } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { environment } from '../environments/environment';
 
 const AppLanguage = 'app-language';
 
@@ -11,7 +12,7 @@ export function appInitializerFactory(translate: TranslateService, injector: Inj
         const locationInitialized = injector.get(LOCATION_INITIALIZED, Promise.resolve(null));
         locationInitialized.then(() => {
             const langToSet = localStorage.getItem(AppLanguage) as string || 'en';
-            translate.setDefaultLang('en');
+            translate.setDefaultLang(environment.defaultLocale);
             translate.use(langToSet).subscribe(() => {
                 console.info(`Successfully initialized '${langToSet}' language.'`);
             }, err => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(translate: TranslateService) {
+    translate.setDefaultLang(environment.defaultLocale);
+    translate.use(environment.defaultLocale);
+  }
 }
