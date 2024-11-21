a6d6df686a8b8cc1e3ca72709ecd053779adfaf8
serhiy.krasovskyy <xhunter74@gmail.com>
2024-11-21 19:36:31 +0200

    [skip ci] Translated date picker.

--- src/app/base-component/app-base.component.ts
+++ src/app/base-component/app-base.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { SpinnerDialogComponent } from '../spinner-dialog/spinner-dialog.component';
 import { StringUtils } from '../utils';
 import { TranslateService } from '@ngx-translate/core';
+import { DateAdapter } from '@angular/material/core';
 
 @Component({
     template: ''
 })
 export class AppBaseComponent implements OnDestroy {
 
+    private readonly dateAdapter = inject<DateAdapter<unknown, unknown>>(DateAdapter);
     protected dialogRef: MatDialogRef<SpinnerDialogComponent>;
 
     constructor(protected dialog: MatDialog, protected translate: TranslateService) {
-
+        this.dateAdapter.setLocale(this.translate.currentLang);
+        this.translate.onLangChange.subscribe(() => {
+            this.dateAdapter.setLocale(this.translate.currentLang);
+        });
     }
 
     ngOnDestroy(): void {
