93e0cc27e48783aa3a1e0c109a9c37f0eba1687b
serhiy.krasovskyy <xhunter74@gmail.com>
2024-11-25 22:25:03 +0200

    [skip ci] Fixed translation. Switched version => 2.0.0.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.powermonitor.my-home.live/api/',
-  version: '1.2.30',
+  version: '2.0.0',
   locales: ['en', 'uk'],
   defaultLocale: 'en'
 };
