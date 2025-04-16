f195ccaaa94f796a7dd0c881dd83a40b48aef250
bitbucket-pipelines <commits-noreply@bitbucket.org>
2025-04-16 16:07:03 +0000

    [skip ci] Updated Powermonitor site version to 2.0.15.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.powermonitor.my-home.live/api/',
-  version: '2.0.14',
+  version: '2.0.15',
   locales: ['en', 'uk'],
   defaultLocale: 'en'
 };
