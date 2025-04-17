7cfb653fa1c26fa57132a952e27b82857c2b706f
serhiy.krasovskyy <xhunter74@gmail.com>
2025-04-17 21:33:09 +0300

    Fixed replace script.

--- replace.build.mjs
+++ replace.build.mjs
@@ -1,21 +1,23 @@
-import { sync } from 'replace-in-file';
-import { version } from "./package.json";
-var buildVersion = version;
+import { replaceInFileSync } from 'replace-in-file';
+import { readFileSync } from 'fs';
+
+const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
+const buildVersion = packageJson.version;
+
 const options = {
     files: 'src/environments/environment.prod.ts',
     from: /version: '(.*)'/g,
-    to: "version: '"+ buildVersion + "'",
+    to: "version: '" + buildVersion + "'",
     allowEmptyPaths: false,
 };
 
 try {
-    let changedFiles = sync(options);
+    let changedFiles = replaceInFileSync(options);
     if (changedFiles == 0) {
         throw "Please make sure that file '" + options.files + "' has \"version: ''\"";
     }
     console.log('Build version set: ' + buildVersion);
-}
-catch (error) {
+} catch (error) {
     console.error('Error occurred:', error);
-    throw error
+    throw error;
 }
