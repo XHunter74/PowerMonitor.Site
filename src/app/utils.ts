e974ac5f7a5d834b5a5a5a2f65438e8b861cdfc2
serhiy.krasovskyy <xhunter74@gmail.com>
2025-04-27 22:41:35 +0300

    Clean code.

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -10,10 +10,6 @@ export function isCurrentMonth(currentDate: Date) {
     return isCurrentMonthResult;
 }
 
-export function compare(a: number | string | Date, b: number | string | Date, isAsc: boolean) {
-    return (a < b ? -1 : (a === b ? 0 : 1)) * (isAsc ? 1 : -1);
-}
-
 export function getStringDate(val: Date) {
     const dateStr = val.getFullYear().toString() + '-' + (val.getMonth() + 1).toString() +
         '-' + val.getDate().toString();
