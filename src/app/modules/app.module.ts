9eb0bca7ef845ccf3c3dc5ddaf53d83f26df1776
serhiy.krasovskyy <xhunter74@gmail.com>
2025-05-01 11:54:57 +0300

    Returned ng-select.

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -22,6 +22,7 @@ import { DateAdapter, MAT_DATE_LOCALE, MAT_DATE_FORMATS } from '@angular/materia
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { appInitializerFactory, HttpLoaderFactory } from './app-initialize.factory';
 import { environment } from '../../environments/environment';
+import { NgSelectModule } from '@ng-select/ng-select';
 import { AuthGuard, OpenGuard } from '../guards/auth.guard';
 import { StoreModule } from '@ngrx/store';
 import { reducers } from '../store/reducers';
@@ -59,6 +60,7 @@ import 'moment/locale/uk';
         deps: [HttpClient]
       }
     }),
+    NgSelectModule,
     StoreModule.forRoot(reducers),
     EffectsModule.forRoot(),
     ServiceWorkerModule.register('ngsw-worker.js', {
