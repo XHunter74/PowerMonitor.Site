24ef2e391451158d44590c85e8cc49276ffb17e2
Serhiy Krasovskyy <xhunter74@gmail.com>
2025-05-02 08:40:53 +0000

    [skip ci] Updated Powermonitor site version to 2.0.53.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.powermonitor.my-home.live/api/',
-  version: '2.0.52',
+  version: '2.0.53',
   locales: ['en', 'uk'],
   defaultLocale: 'en'
 };
