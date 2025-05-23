4a1df0033056a10dee66d46598cbdafd4dd88896
serhiy.krasovskyy <xhunter74@gmail.com>
2025-05-23 14:40:31 +0300

    Fixed test.

--- tests/store/effects/power-failures.monthly.effects.spec.ts
+++ tests/store/effects/power-failures.monthly.effects.spec.ts
@@ -4,6 +4,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Observable } from 'rxjs';
 import { PowerFailuresMonthlyEffects } from '../../../src/app/store/effects/power-failures.monthly.effects';
 import { AuthService } from '../../../src/app/services/auth.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('PowerFailuresMonthlyEffects', () => {
     let actions$: Observable<any> = new Observable<any>();
@@ -11,11 +12,7 @@ describe('PowerFailuresMonthlyEffects', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [
-                HttpClientTestingModule,
-                // Add TranslateModule to provide TranslateService and TranslateStore
-                require('@ngx-translate/core').TranslateModule.forRoot(),
-            ],
+            imports: [HttpClientTestingModule, TranslateModule.forRoot()],
             providers: [
                 PowerFailuresMonthlyEffects,
                 provideMockActions(() => actions$),
